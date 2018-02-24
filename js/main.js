f661d90e5433edb42f6dce80339f88ad75215e4f
WestedCrean <wikflis@gmail.com>
2018-02-24 20:11:11 +0100

    Dominik jest bogiem navbarow

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
 
-    var navbar = $("#navigator");
-    AdjustHeader(); // Incase the user loads the page from halfway down (or something);
-    $(window).scroll(function() {
-        AdjustHeader();
-    });
-
-    function AdjustHeader(){
-        if ($(window).scrollTop() > 200) {
-            if (!navbar.hasClass("position-fixed")) {
-                navbar.addClass("position-fixed");
-            }
-        } else {
-            navbar.removeClass("position-fixed");
-        }
-    }
-
 
 
 });
