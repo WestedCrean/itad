9693d75fa9ce5d7f77e44a935dbead57d7429690
WestedCrean <wikflis@gmail.com>
2018-02-28 13:56:48 +0100

    cleaned up js, added jquery touch support to bootstrap carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
+    //Sticky navbar
+    $(function(){
+        $(window).scroll(function(){
+            var winTop = $(window).scrollTop();
+            if(winTop >= 30){
+                $("body").addClass("sticky-header");
+                ("#itadlogo-navbar").css("visibility","visible");
+            }else{
+                $("body").removeClass("sticky-header");
+                $("#itadlogo-navbar").css("visibility","hidden");
+            }
+        });
+    });
+    //Smooth nav link scrolling
+        $('a[href^="#"]').on('click',function (e) {
+            e.preventDefault();
+
+            var target = this.hash;
+            var $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 600, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+
+    $('.carousel').bcSwipe({ threshold: 50 });
 });
 
