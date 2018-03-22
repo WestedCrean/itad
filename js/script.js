077e4dff47371e25221746b1844ee2be2df01732
WestedCrean <wikflis@gmail.com>
2018-03-22 22:21:32 +0100

    added speakers, new partners

--- js/script.js
+++ js/script.js
@@ -168,5 +168,25 @@ $('a[href*="#"]')
     $('.navbar-wrapper').hide();
 }
 
+// Accordions
+var acc = document.getElementsByClassName("intro-chevron");
+
+for (var i = 0, acclength = acc.length; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
 });
 
